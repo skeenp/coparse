a1a8240ab4c4f061a95a3ba0ef3e3e5d7d0d84dd
Paul Skeen <plesk@bigpond.com>
2018-11-16 15:36:55 +1100

    House keeping
    Tidied up code
    Spelling corrections
    Package version update

--- index.js
+++ index.js
@@ -420,28 +420,21 @@ function convertUtmToLatLng(
   var y = UTMNorthing;
   var ZoneNumber = UTMZoneNumber;
   var ZoneLetter = UTMZoneLetter;
-
-  if ("N" !== ZoneLetter) {
-    y -= 10000000.0;
-  }
-
+  if ("N" !== ZoneLetter) y -= 10000000.0;
   var LongOrigin = (ZoneNumber - 1) * 6 - 180 + 3;
-
   var eccPrimeSquared = eccSquared / (1 - eccSquared);
-
-  M = y / 0.9996;
+  var M = y / 0.9996;
   var mu = M / (a * (1 - eccSquared / 4 - (3 * eccSquared * eccSquared) / 64 - (5 * eccSquared * eccSquared * eccSquared) / 256));
   var phi1Rad = mu + ((3 * e1) / 2 - (27 * e1 * e1 * e1) / 32) * Math.sin(2 * mu) + ((21 * e1 * e1) / 16 - (55 * e1 * e1 * e1 * e1) / 32) * Math.sin(4 * mu) + ((151 * e1 * e1 * e1) / 96) * Math.sin(6 * mu);
-  var phi1 = toDegrees(phi1Rad);
   var N1 = a / Math.sqrt(1 - eccSquared * Math.sin(phi1Rad) * Math.sin(phi1Rad));
   var T1 = Math.tan(phi1Rad) * Math.tan(phi1Rad);
   var C1 = eccPrimeSquared * Math.cos(phi1Rad) * Math.cos(phi1Rad);
   var R1 = (a * (1 - eccSquared)) / Math.pow(1 - eccSquared * Math.sin(phi1Rad) * Math.sin(phi1Rad), 1.5);
   var D = x / (N1 * 0.9996);
   var Lat = phi1Rad - ((N1 * Math.tan(phi1Rad)) / R1) * ((D * D) / 2 - ((5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * eccPrimeSquared) * D * D * D * D) / 24 + ((61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * eccPrimeSquared - 3 * C1 * C1) * D * D * D * D * D * D) / 720);
-  Lat = toDegrees(Lat);
+  var Lat = toDegrees(Lat);
   var Long = (D - ((1 + 2 * T1 + C1) * D * D * D) / 6 + ((5 - 2 * C1 + 28 * T1 - 3 * C1 * C1 + 8 * eccPrimeSquared + 24 * T1 * T1) * D * D * D * D * D) / 120) / Math.cos(phi1Rad);
-  Long = LongOrigin + toDegrees(Long);
+  var Long = LongOrigin + toDegrees(Long);
   return {
     lat: Lat,
     lng: Long
